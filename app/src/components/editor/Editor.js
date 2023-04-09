f2333033ecd3fb9c7f91f3a4df035e453c4ae603
Elena Brazhnikova <lenovosibirsk@mail.ru>
2023-04-09 21:56:07 +0700

    add editor pages

---
+++ app/src/components/editor/Editor.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import React, { Component } from "react";
+
+export default class Editor extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pageList: [],
+      newPageName: "",
+    };
+    this.createNewPage = this.createNewPage.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadPageList();
+  }
+
+  loadPageList() {
+    axios.get("./api").then((res) => this.setState({ pageList: res.data }));
+  }
+
+  createNewPage() {
+    axios
+      .post("./api/createNewPage.php", { name: this.state.newPageName })
+      .then((res) => console.log(res));
+  }
+
+  render() {
+    const { pageList } = this.state;
+    const pages = pageList.map((page, i) => {
+      return <h1 key={i}>{page}</h1>;
+    });
+    return (
+      <>
+        <input
+          onChange={(e) => {
+            this.setState({ newPageName: e.target.value });
+          }}
+          type="text"
+          placeholder="Введите название страницы"
+        />
+        <button onClick={this.createNewPage}>Создать страницу</button>
+        {pages}
+      </>
+    );
+  }
+}
--- app/src/main.js
+++ app/src/main.js
@@ -1,3 +1,35 @@
-import $ from "jquery";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Editor from "./components/editor";
 
-$("h1").fadeOut();
+const root = createRoot(document.getElementById("root"));
+root.render(<Editor />);
+
+// function getPageList() {
+//   $("h1").remove();
+//   $.get(
+//     "./api",
+//     (data) => {
+//       data.forEach((file) => {
+//         $("body").append(`<h1>${file}</h1>`);
+//       });
+//     },
+//     "JSON"
+//   );
+// }
+
+// getPageList();
+
+// $("button").click(() => {
+//   $.post(
+//     "./api/createNewPage.php",
+//     {
+//       name: $("input").val(),
+//     },
+//     () => {
+//       getPageList();
+//     }
+//   ).fail(() => {
+//     alert("Страница уже существует!");
+//   });
+// });
